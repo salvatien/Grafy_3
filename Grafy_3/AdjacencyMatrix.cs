fe47c69a5ca66d4a71b565f7d26015b3cfc9d754
salva <tomasz.kleiner@gmail.com>
2017-05-09 15:43:31 +0200

    Dodaj pliki projektów.

---
+++ Grafy_3/AdjacencyMatrix.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafy_3
+{
+    class AdjacencyMatrix
+    {
+
+        //nasza macierz :)
+        public int[,] AdjacencyArray;
+
+
+
+
+        //zerujemy na poczatek
+        public AdjacencyMatrix(int n)
+        {
+            AdjacencyArray = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    AdjacencyArray[i, j] = 0;
+                }
+            }
+        }
+
+
+
+        //rysujemy zranomizowany graf - nawet nie starajcie sie tego zrozumiec
+        //dluuugo to naprawialam i sama nie jestem pewna jak, ale dziala
+        public void Display(Canvas MyCanvas, int[] TabOfInt, bool randomm, bool draw)
+        {
+
+            int max = TabOfInt.Length - 1;
+            for (int i = 0; i < max + 1; i++)
+            {
+                for (int j = 0; j < max + 1; j++)
+                {
+                    AdjacencyArray[i, j] = 0;
+                }
+            }
+
+            for (int i = 0; i <= max; i++)
+            {
+                int value = TabOfInt[i];
+                if (value != 0)
+                {
+                    for (int x = 1; x <= value; x++)
+                    {
+                        int[] Tab = new int[max + 1];
+                        for (int k = 0; k <= max; k++)
+                        {
+                            Tab[k] = 0;
+                        }
+                        int j = FindMax(i, TabOfInt, Tab);
+                        Tab[j] = 1;
+                        bool done = false;
+                        while (done == false)
+                        {
+                            if (AdjacencyArray[i, j] == 0 && i != j)
+                            {
+                                AdjacencyArray[i, j] = 1;
+                                AdjacencyArray[j, i] = 1;
+                                TabOfInt[j]--;
+                                done = true;
+                            }
+                            else
+                            {
+                                j = FindMax(j, TabOfInt, Tab);
+                                Tab[j] = 1;
+                            }
+                        }
+                    }
+                    TabOfInt[i] = 0;
+                }
+
+            }
+
+            
+
+
+
+
+            //wyswietlanie
+            if (draw)
+                DrawGraph(AdjacencyArray.GetLength(0), MyCanvas);
+        }
+
+        //potrzebne do trzenia macierzy - nie probujcie zrozumiec
+        public int FindMax(int j, int[] Tab, int[] Tabb)
+        {
+            int max = Tab.Length - 1;
+            int ret = -1;
+            if (max == 0)
+            {
+                ret = 0;
+            }
+            else
+            {
+                if (Tabb[0] == 0)
+                {
+                    for (int i = 0; i <= max; i++)
+                    {
+                        if (ret == -1)
+                        {
+                            ret = 0;
+                        }
+                        else { if (Tab[i] > Tab[ret] && Tabb[i] == 0) ret = i; };
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i <= max; i++)
+                    {
+                        if (ret == -1)
+                        {
+                            ret = 1;
+                        }
+                        else { if (Tab[i] > Tab[ret] && Tabb[i] == 0) ret = i; };
+                    }
+                }
+
+            }
+            return ret;
+        }
+
+
+
+
+
+        //rysowanie skopiowane z zestawu 1
+        public void DrawGraph(int num_v, Canvas MyCanvas)
+        {
+            MyCanvas.Children.Clear();
+
+            var width = MyCanvas.Width;
+            var height = MyCanvas.Height;
+
+            Ellipse myEllipse = new Ellipse();
+            myEllipse.Height = 400;
+            myEllipse.Width = 400;
+            myEllipse.Fill = Brushes.Transparent;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.LightGray;
+            Canvas.SetLeft(myEllipse, width / 2 - 200);
+            Canvas.SetTop(myEllipse, height / 2 - 200);
+            MyCanvas.Children.Add(myEllipse);
+
+            var r = 200;    //radius
+
+            var x_m = width / 2;    //x middle
+            var y_m = height / 2;   //y middle
+
+
+            for (int i = 1; i <= num_v; i++)
+            {
+                var angle = (2 * Math.PI) / num_v * i;
+
+                var x_oc = r * Math.Cos(angle) + x_m;   //x on cirlce
+                var y_oc = r * Math.Sin(angle) + y_m;   //y on circle
+
+                Ellipse smallPoint = new Ellipse();
+                smallPoint.Height = 8;
+                smallPoint.Width = 8;
+                smallPoint.Fill = Brushes.Black;
+                smallPoint.StrokeThickness = 1;
+                smallPoint.Stroke = Brushes.Black;
+                Canvas.SetLeft(smallPoint, x_oc - 3);
+                Canvas.SetTop(smallPoint, y_oc - 3);
+
+                TextBlock smallPointNumber = new TextBlock();
+                smallPointNumber.Text = i.ToString();
+                smallPointNumber.RenderTransform = new TranslateTransform
+                {
+                    X = (r + 10) * Math.Cos(angle) + x_m,
+                    Y = (r + 15) * Math.Sin(angle) + y_m - 8
+                };
+
+                for (int j = i; j <= AdjacencyArray.GetLength(0); j++)
+                {
+                    if (AdjacencyArray[i - 1, j - 1] != 0)
+                    {
+                        var angle_2 = (2 * Math.PI) / num_v * j;
+
+                        var x_oc_2 = r * Math.Cos(angle_2) + x_m;   //x on cirlce
+                        var y_oc_2 = r * Math.Sin(angle_2) + y_m;   //y on circle
+
+                        Line myLine = new Line();
+                        myLine.Stroke = Brushes.Black;
+                        myLine.StrokeThickness = 3;
+                        myLine.X1 = x_oc;
+                        myLine.Y1 = y_oc;
+                        myLine.X2 = x_oc_2;
+                        myLine.Y2 = y_oc_2;
+
+                        MyCanvas.Children.Add(myLine);
+                    }
+                }
+
+                MyCanvas.Children.Add(smallPoint);
+                MyCanvas.Children.Add(smallPointNumber);
+
+            }
+        }
+
+        //budujemy graf hamiltonowski o zadanej liczbie krawedzi
+        public void Display(Canvas MyCanvas, int v)
+        {
+            //cykl eulera
+            int[] done = new int[v];
+            for (int i = 0; i < v; i++)
+            {
+                done[i] = 0;
+            }
+            int ile = 1;
+            Random r = new Random();
+            int aktualny = r.Next(0, v);
+            int pierwszy = aktualny;
+            done[aktualny] = 1;
+            int nowy;
+            while (ile != v)
+            {
+                nowy = r.Next(0, v);
+                while (nowy != aktualny && done[nowy] == 0)
+                {
+                    AdjacencyArray[nowy, aktualny] = 1;
+                    AdjacencyArray[aktualny, nowy] = 1;
+                    aktualny = nowy;
+                    ile++;
+                    done[nowy] = 1;
+
+                }
+
+            }
+            AdjacencyArray[pierwszy, aktualny] = 1;
+            AdjacencyArray[aktualny, pierwszy] = 1;
+
+            //dodajemy kolejne krawedzie
+            double ilemax = v * (v - 1) / 2;
+            double p = r.Next(35, 81);
+            double ilemabyc = ilemax * p / (double)100.0;
+            int iledodac = (int)ilemabyc - v;
+            if (iledodac > 0)
+            {
+                ile = 0;
+                while (ile != iledodac)
+                {
+                    aktualny = r.Next(0, v);
+                    nowy = r.Next(0, v);
+                    while (nowy != aktualny && AdjacencyArray[nowy, aktualny] == 0)
+                    {
+                        AdjacencyArray[nowy, aktualny] = 1;
+                        AdjacencyArray[aktualny, nowy] = 1;
+                        ile++;
+                    }
+                }
+            }
+
+
+            //wyswietlanie
+            DrawGraph(AdjacencyArray.GetLength(0), MyCanvas);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Grafy_3/MainWindow.xaml.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grafy_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        private AdjacencyMatrix adjacencyMatrix;
+        private int w = 0;
+        private int s = 0;
+        private int no = 1;
+        private int used = 0;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        //budujemy graf eulerowski i znajdujemy cykl
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            
+
+            //budujemy
+            if (NumberV.Text != "0")
+            {
+                var howManyVertex = Int32.Parse(NumberV.Text);
+                NumberV.Background = Brushes.White;
+                int maxDegree = howManyVertex - 1;
+                int maxRand = maxDegree / 2;
+
+                Random randNum = new Random();
+                int[] TabOfInt = new int[howManyVertex];            //tablica do przechowywania stopni wierzcholkow
+                for (int i = 0; i< howManyVertex; i++)
+                {
+                    TabOfInt[i] = randNum.Next(1, maxRand+1) * 2;           // wypelniamy tablice wartosciami (tylko liczby parzyste!)
+                }
+
+
+                AdjacencyMatrix adjacencyMatrix = new AdjacencyMatrix(TabOfInt.Length);
+                adjacencyMatrix.Display(MyCanvas, TabOfInt, true, false);
+
+                //sprawdzanie NSS
+                int num_of_v = TabOfInt.Length;
+                int v = num_of_v;
+
+                Stack<int> stos = new Stack<int>();
+                int cn = 0;
+                int[] c = new int[v];
+                for (int i = 0; i < v; i++)
+                {
+                    c[i] = 0;
+                }
+                for (int i = 0; i < v; i++)
+                {
+                    if (c[i] > 0)
+                    {
+                        continue;
+                    }
+                    cn++;
+                    stos.Push(i);
+                    c[i] = cn;
+                    while (stos.Count > 0)
+                    {
+                        int vv = stos.Pop();
+                        List<int> neighbours = new List<int>();
+                        for (int j = 0; j < v; j++)
+                        {
+                            if (adjacencyMatrix.AdjacencyArray[vv, j] == 1)
+                            {
+                                neighbours.Add(j);
+                            }
+                        }
+                        for (int j = 0; j < neighbours.Count; j++)
+                        {
+                            if (c[neighbours[j]] > 0)
+                            {
+                                continue;
+                            }
+                            stos.Push(neighbours[j]);
+                            c[neighbours[j]] = cn;
+                        }
+
+
+
+
+                    }
+
+
+                    if (c.Count(x => x == cn) == v)
+                        break;
+                }
+
+                int max = 0, maxval = 0;
+                for (int i = 1; i <= cn; i++)
+                {
+                    if (c.Count(x => x == i) > max)
+                    {
+                        max = c.Count(x => x == i);
+                        maxval = i;
+                    }
+                }
+
+                string ciag = "";
+                List<int> doWypisania = new List<int>();
+                int ile = 0;
+                for (int i = 0; i < v; i++)
+                {
+                    if (c[i] == maxval)
+                    {
+                        doWypisania.Add(i);
+                        ile++;
+                    }
+                }
+
+                for (int i = 0; i < num_of_v; i++)
+                {
+                    for (int j = i + 1; j < num_of_v; j++)
+                    {
+                        if (doWypisania.Contains(i) && doWypisania.Contains(j))
+                        {
+                            //////////////////////////
+                        }
+                        else
+                        {
+                            adjacencyMatrix.AdjacencyArray[i, j] = 0;
+                            adjacencyMatrix.AdjacencyArray[j, i] = 0;
+                        }
+                    }
+                }
+
+                adjacencyMatrix.DrawGraph(num_of_v, MyCanvas);
+
+
+
+
+
+
+                //cykl
+                Random r = new Random();
+                string OutPut;
+                Stack stosik = new Stack();
+                int vvv = r.Next(0, num_of_v);
+                Euler(0, stosik, num_of_v, adjacencyMatrix.AdjacencyArray);
+
+                //wypisz na ekran
+                object[] tab = stosik.ToArray();
+                string wypisz = "";
+                for (int i = 0; i < tab.Length; i++)
+                {
+                    wypisz = wypisz + tab[i].ToString() + ", ";
+                }
+                NumberV.Text = wypisz;
+            }
+            else
+            {
+                NumberV.Background = Brushes.OrangeRed;
+            }
+
+
+        }
+
+        //znajdowanie cyklu eulera
+        public void Euler(int v, Stack stos, int num_of_v, int[,] AdjacencyArray)
+        {
+            for (int i = 0; i < num_of_v; i++)
+            {
+                if (AdjacencyArray[v, i] == 1)
+                {
+                    AdjacencyArray[v, i] = 0;
+                    AdjacencyArray[i, v] = 0;
+                    Euler(i, stos, num_of_v, AdjacencyArray);
+                }
+            }
+            stos.Push(v + 1);
+        }
+
+        /*
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            var v = Int32.Parse(Ver.Text);
+            var k = Int32.Parse(kkk.Text);
+            int[] TabOfInt = new int[v];
+            for (int i = 0; i < v; i++)
+            {
+                TabOfInt[i] = (int)k;
+            }
+
+
+            //Ta zmienna mówi nam czy ciąg jest grafuiczny czy nie jest
+            bool TrueOrFalse = false;
+            //znacznik konca petli
+            bool EndLoop = false;
+            //idneks ostatniego elementu tablicy
+            int max = TabOfInt.Length - 1;
+
+            //tu się dzieja rzeczy
+            while (EndLoop == false)
+            {
+                //bierzemy ostani element i sprawdzamy czy nie jest on za duzy jak np. w ciagu 1,2,8 
+                //bo wtedy nie mozemy go odjac i z miejsca taki ciag nie jest graficzny
+                Array.Sort(TabOfInt);
+                int value = TabOfInt[max];
+                if (value > max)
+                {
+                    TrueOrFalse = false;
+                    EndLoop = true;
+                }
+                //jesli sie zgadza wszytko to po kolei odejmujemy i ustawiamy value na 0
+                else
+                {
+                    for (int i = max - 1; i >= max - value; i--)
+                    {
+                        TabOfInt[i]--;
+                    }
+                    TabOfInt[max] = 0;
+                    //sortowanie
+                    Array.Sort(TabOfInt);
+                    //jesli ostatnia, czyli najwieksza liczba, to 0, to znaczy ze jest koniec
+                    //tutaj zaznaczamy EndLoop na true (konczymy petle)
+                    //zakladamy tez ze mamy same 0 - wtedy ciag jest graficzny - czyli TrueOrFalse = true
+                    //potem w petli for sprawdzamy czy nie mamy jakiego (-1) - ciag nie jest graficzny
+                    if (TabOfInt[max] == 0)
+                    {
+                        EndLoop = true;
+                        TrueOrFalse = true;
+                        for (int i = 0; i <= max; i++)
+                        {
+                            if (TabOfInt[i] < 0) TrueOrFalse = false;
+                        }
+                    }
+                }
+
+            }
+
+            //wyswietlamy odpowiedni komunikat w miejscu, gdzie byl ciag
+            if (TrueOrFalse == true)
+            {
+                kkk.Background = Brushes.White;
+                Ver.Background = Brushes.White;
+
+                for (int i = 0; i < v; i++)
+                {
+                    TabOfInt[i] = (int)k;
+                }
+
+                AdjacencyMatrix adjacencyMatrix = new AdjacencyMatrix(TabOfInt.Length);
+                adjacencyMatrix.Display(MyCanvas, TabOfInt, true, true);
+
+
+            }
+
+
+            else
+            {
+                kkk.Background = Brushes.OrangeRed;
+                Ver.Background = Brushes.OrangeRed;
+            }
+
+
+
+        }
+
+    */
+     
+    }
+}
